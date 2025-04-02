61ca7d837311dc640902dfa714665042d8251163
xvh422 <joey.carter555@gmail.com>
2025-04-02 12:24:45 +0100

    added voting to homepage

--- utils/api.js
+++ utils/api.js
@@ -28,6 +28,14 @@ function getArticleById(article_id) {
   });
 }
 
+function patchArticleVotes(article_id, increase = true) {
+  let url = `https://nc-news-5066.onrender.com/api/articles/${article_id}`;
+  const requestBody = { inc_votes: increase ? 1 : -1 };
+  return axios.patch(url, requestBody).then(({ data }) => {
+    return data.article;
+  });
+}
+
 function getCommentsByArticleId(article_id, page) {
   let url = `https://nc-news-5066.onrender.com/api/articles/${article_id}/comments?limit=10&p=${page}`;
   return axios.get(url).then(({ data }) => {
@@ -40,5 +48,6 @@ export {
   getUserByUsername,
   getAllArticles,
   getArticleById,
+  patchArticleVotes,
   getCommentsByArticleId,
 };
